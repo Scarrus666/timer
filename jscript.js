9f32a7676bf4eed09c0a286e0599dfb55aebb381
Scarrus666 <scarrus@gmail.com>
2023-06-12 11:57:29 +0200

    changes to js file

---
+++ jscript.js
@@ -0,0 +1,12 @@
+var timer;
+var ele = document.getElementById('timer');
+
+(function()
+    {
+        var sec = 0;
+        timer = setinterval(()=>
+        {
+            ele.innerHTML = '00:' + sec;
+            sec ++;
+        },   1000) //each 1 second
+    })()
