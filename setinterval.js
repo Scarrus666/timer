4b5da093453b5ffa417a736fe32c56015c08da79
Scarrus666 <scarrus@gmail.com>
2023-06-12 16:23:20 +0200

    some changes and new method

---
+++ setinterval.js
@@ -0,0 +1,19 @@
+var timer;
+var ele = document.getElementById("timer");
+
+(function()
+    {
+        var sec = 0;
+        timer = setinterval(()=>
+        {
+            ele.innerHTML = '00:'+sec;
+            sec ++;
+        },   1000) //each 1 second
+    }
+)
+()
+
+function pause()
+    {
+        clearInterval(timer);
+    }
